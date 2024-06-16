9879e4365357d857bed66c52a33c3d4fe34a36cc
ana <anamariagarciah12@gmail.com>
2024-06-16 16:03:24 -0500

    Create registration form design

---
+++ pages/login.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-form>
+    <Input label="Nombre de usuario" />
+    <Input label="Nombre completo" />
+    <Input label="Contraseña" />
+    <Button label="Iniciar sesion" />
+  </v-form>
+</template>
