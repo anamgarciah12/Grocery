8e47af82570d8c28c2a637bd68305beabd500908
ana <anamariagarciah12@gmail.com>
2024-06-16 15:55:46 -0500

    Create component input

---
+++ components/Input.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-text-field
+    :label="label"
+    :dark="dark"
+    :rules="rules"
+    v-model="localModel"
+    rounded
+    :style="width && `max-width:${width}`"
+    outlined
+    :hideDetails="hideDetails"
+    :prependInnerIcon="prependInnerIcon && `mdi-${prependInnerIcon}`"
+    dense
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    prependInnerIcon: {
+      type: String,
+      default: "",
+    },
+    model: {
+      type: [String, Number],
+      default: null,
+    },
+    hideDetails: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: "Label",
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+    width: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      localModel: null,
+    };
+  },
+  watch: {
+    localModel(val) {
+      this.$emit("update:model", val);
+    },
+    model(val) {
+      this.localModel = val;
+    },
+  },
+};
+</script>
