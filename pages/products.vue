b9d6ef4b402598b8b0664c78f66c8f5b2cda2280
ana <anamariagarciah12@gmail.com>
2024-06-16 16:51:46 -0500

    Add title to view and apply primary color to buttons

--- pages/products.vue
+++ pages/products.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app id="inspire">
     <v-main>
+      <v-toolbar flat color="#ff000000">
+        <h1>Productos</h1>
+        <v-spacer />
+        <Button label="Ver lista de compras" color="#F16E06" />
+      </v-toolbar>
       <v-container>
         <v-row>
           <v-col v-for="n in 6" :key="n" cols="4">
@@ -12,7 +17,11 @@
               <v-card-title>Cafe Badilico</v-card-title>
               <v-card-subtitle>$ 5000</v-card-subtitle>
               <v-card-actions>
-                <Button :block="true" label="Agregar a la lista de compras" />
+                <Button
+                  :block="true"
+                  label="Agregar a la lista de compras"
+                  color="#F16E06"
+                />
               </v-card-actions>
             </v-card>
           </v-col>
